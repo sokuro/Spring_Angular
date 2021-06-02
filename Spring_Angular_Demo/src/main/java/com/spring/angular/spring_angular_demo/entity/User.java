9e59c03ec7bc3df5c6989f6185d25a03e6dd23f0
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-06-02 15:16:52 +0200

    Entity: Constructor extended

--- Spring_Angular_Demo/src/main/java/com/spring/angular/spring_angular_demo/entity/User.java
+++ Spring_Angular_Demo/src/main/java/com/spring/angular/spring_angular_demo/entity/User.java
@@ -20,6 +20,11 @@ public class User {
 
     private String email;
 
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public User() {
     }
 
