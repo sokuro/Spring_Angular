0793df3492fa29dec8f412fcd44f0597c26e67cf
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-06-02 15:16:59 +0200

    04_Bootstrap the App

--- Spring_Angular_Demo/src/main/java/com/spring/angular/spring_angular_demo/SpringAngularDemoApplication.java
+++ Spring_Angular_Demo/src/main/java/com/spring/angular/spring_angular_demo/SpringAngularDemoApplication.java
@@ -1,7 +1,13 @@
 package com.spring.angular.spring_angular_demo;
 
+import com.spring.angular.spring_angular_demo.entity.User;
+import com.spring.angular.spring_angular_demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringAngularDemoApplication {
@@ -10,4 +16,14 @@ public class SpringAngularDemoApplication {
         SpringApplication.run(SpringAngularDemoApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
