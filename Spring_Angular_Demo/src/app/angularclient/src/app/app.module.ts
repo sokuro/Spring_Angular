01bcfb82114fa8acf13de67eba013abcb4dd0972
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-06-04 12:44:59 +0200

    08) adding Angular UserListComponent

--- Spring_Angular_Demo/src/app/angularclient/src/app/app.module.ts
+++ Spring_Angular_Demo/src/app/angularclient/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
